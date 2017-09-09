465b27bc66531c9e94d83f471ef25e5e6f4b82b1
qzchenwl <qzchenwl@gmail.com>
2017-09-09 11:26:33 +0800

    support sync talos query `%%talos2`

--- dotfiles/jupyter/.ipython/profile_default/startup/99-talos.py
+++ dotfiles/jupyter/.ipython/profile_default/startup/99-talos.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import time
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic)
 
@@ -14,6 +15,18 @@ class TalosMagics(Magics):
 
         engine, dsn = line.split(' ')
         return TalosWidget(engine=engine, dsn=dsn, sql=cell)
+
+    @cell_magic
+    def talos2(self, line, cell):
+        "my cell magic"
+
+        engine, dsn = line.split(' ')
+        w = TalosWidget(engine=engine, dsn=dsn, sql=cell)
+        display(w)
+        while w.update():
+            time.sleep(1)
+        return w
+
 
 # In order to actually use these magics, you must register them with a
 # running IPython.  This code must be placed in a file that is loaded once

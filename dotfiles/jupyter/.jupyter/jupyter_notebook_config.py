ec5955450db6efaaa2aa20e07d1bdde7c40c6aa9
qzchenwl <qzchenwl@gmail.com>
2017-09-25 15:00:39 +0800

    iopub_date_rate_limit

--- dotfiles/jupyter/.jupyter/jupyter_notebook_config.py
+++ dotfiles/jupyter/.jupyter/jupyter_notebook_config.py
@@ -2,4 +2,4 @@ import os
 
 c.NotebookApp.notebook_dir = '/home/jovyan/work'
 c.NotebookApp.token = os.environ.get('NOTEBOOK_TOKEN', 'data-science')
-
+c.NotebookApp.iopub_data_rate_limit = 10000000
